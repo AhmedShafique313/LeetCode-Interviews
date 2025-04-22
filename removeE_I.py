27e12b5a59b23006b607ca13310ce5034e43e12f
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2025-04-22 18:30:29 +0500

    Removing Even Integers from the List

--- removeE_I.py
+++ removeE_I.py
@@ -1,9 +1,11 @@
 a = [1, 9, 3, 5, 7, 8]
 
 def removal_even_integers(arr):
-    # for i in arr:
-    #     if i%2!=0:
-    #         return i
-    return [i for i in arr if i%2!=0]
+    result = []
+    for i in arr:
+        if i % 2!=0:
+            result.append(i)
+    return result
+
 
 print(removal_even_integers(a))
